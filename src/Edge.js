5df9f789a83a46a330da51fee9ab69c4629175fa
rot1024 <aayhrot@gmail.com>
2018-01-17 15:13:27 +0900

    implement Edge component

---
+++ src/Edge.js
@@ -0,0 +1,77 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Edge = ({
+  className,
+  id,
+  strokeColor = "#000",
+  strokeWidth = 1,
+  x1,
+  x2,
+  y1,
+  y2,
+  shadowRadius = 0,
+  style
+}) => {
+  const w = Math.abs(x1 - x2);
+  const h = Math.abs(y1 - y2);
+  const bx = Math.min(x1, x2);
+  const by = Math.min(y1, y2);
+  const ax = w / 2 + bx;
+  const d = strokeWidth;
+
+  return (
+    <div
+      className={className}
+      id={id}
+      style={{
+        display: "inline-block",
+        width: `${w}px`,
+        height: `${h}px`,
+        overflow: "visible",
+        ...style
+      }}>
+      <svg
+        style={{ margin: `${-d}px 0 0 ${-d}px` }}
+        width={w + d * 2}
+        height={h + d * 2}
+        xmlns="http://www.w3.org/2000/svg">
+        {shadowRadius > 0 && (
+          <defs>
+            <filter id="shadow" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+              <feComponentTransfer in="SourceAlpha">
+                <feFuncR type="discrete" tableValues="0" />
+                <feFuncG type="discrete" tableValues="0" />
+                <feFuncB type="discrete" tableValues="0" />
+              </feComponentTransfer>
+              <feGaussianBlur stdDeviation={2} result="r" />
+              <feComposite in="SourceGraphic" in2="r" operator="over" />
+            </filter>
+          </defs>
+        )}
+        <path
+          fill="none"
+          stroke={strokeColor}
+          strokeWidth={strokeWidth}
+          strokeLinecap="round"
+          filter={shadowRadius > 0 && "url(#shadow)"}
+          d={`M${x1 - bx + d} ${y1 - by + d} C${ax} ${y1 - by + d},${ax} ${y2 - by + d},${x2 - bx + d} ${y2 - by + d}`} />
+      </svg>
+    </div>
+  );
+};
+
+Edge.propTypes = {
+  className: PropTypes.string,
+  id: PropTypes.string,
+  shadowRadius: PropTypes.number,
+  strokeColor: PropTypes.string,
+  strokeWidth: PropTypes.number,
+  style: PropTypes.object,
+  x1: PropTypes.number,
+  x2: PropTypes.number,
+  y1: PropTypes.number,
+  y2: PropTypes.number,
+};
+
+export default Edge;
---
+++ src/stories/Edge.stories.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import Component from "../Edge";
+
+export default () => {
+
+  storiesOf("Edge", module)
+    .add("default", () => (
+      <div>
+        <Component
+          x1={0}
+          y1={0}
+          x2={100}
+          y2={100} />
+        <Component
+          x1={100}
+          y1={0}
+          x2={0}
+          y2={100}
+          strokeColor="red"
+          strokeWidth={5}
+          shadowRadius={2} />
+        <Component
+          x1={0}
+          y1={100}
+          x2={100}
+          y2={0}
+          strokeColor="blue"
+          strokeWidth={20} />
+        <Component
+          x1={100}
+          y1={100}
+          x2={0}
+          y2={0}
+          strokeColor="rgba(0,255,0,0.5)"
+          strokeWidth={20} />
+        <Component
+          x1={200}
+          y1={100}
+          x2={0}
+          y2={0}
+          strokeColor="rgba(255,0,255,0.5)"
+          strokeWidth={20} />
+      </div>
+    ));
+
+};
