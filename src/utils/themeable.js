2ea72a6395965b152071a6897eedc97ded15c88f
rot1024 <aayhrot@gmail.com>
2018-01-18 21:35:08 +0900

    add react-themeable

---
+++ src/utils/themeable.js
@@ -0,0 +1,54 @@
+import themeable from "react-themeable";
+
+const enhancedThemeable = (defaultStyleName, theme, className, style) => {
+  const t = themeable(theme);
+  let key = 1;
+
+  return (...sn) => {
+    let styleNames = sn;
+    let moreClassName;
+    let moreStyle;
+
+    if (typeof sn[0] === "object") {
+      styleNames = sn[0].styleNames;
+      moreClassName = sn[0].className;
+      moreStyle = sn[0].style;
+    }
+
+    const props = t(key++, ...styleNames);
+
+    if (className) {
+      if (props.className) {
+        props.className = `${props.className} ${className}`;
+      } else {
+        props.className = className;
+      }
+    }
+
+    if (moreClassName) {
+      props.className = `${props.className} ${moreClassName}`;
+    }
+
+    if (style) {
+      if (props.style) {
+        props.style = {
+          ...props.style,
+          ...style
+        };
+      } else {
+        props.style = style;
+      }
+    }
+
+    if (moreStyle) {
+      props.style = {
+        ...props.style,
+        ...moreStyle
+      };
+    }
+
+    return props;
+  };
+};
+
+export default enhancedThemeable;
